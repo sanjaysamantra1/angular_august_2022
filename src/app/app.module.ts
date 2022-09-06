b32fe2b8e3c5fb2cb732e8eb494ec0728a3a92de
Sanjay Samantra <sanjaysamantra1@gmail.com>
2022-09-06 21:41:28 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CenterComponent } from './center/center.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
+  // components, directives , pipes
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CenterComponent,
+    FooterComponent,
+    NavbarComponent,
+    CarouselComponent,
   ],
+  // modules
   imports: [
     BrowserModule
   ],
+  // Services/ Injectables
   providers: [],
+  // components to bootstrap/load
   bootstrap: [AppComponent]
 })
 export class AppModule { }
