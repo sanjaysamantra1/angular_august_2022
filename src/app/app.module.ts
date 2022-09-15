ae23c8d84ba6f1fbd116523df9f85089ecd6fab7
Sanjay Samantra <sanjaysamantra1@gmail.com>
2022-09-15 22:01:24 +0530

    15 sept

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -7,6 +10,13 @@ import { CenterComponent } from './center/center.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CarouselComponent } from './carousel/carousel.component';
+import { LoginModule } from './login/login.module';
+import { DatabindingComponent } from './databinding/databinding.component';
+import { DirectivesComponent } from './directives/directives.component';
+import { ProductsComponent } from './products/products.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { NumberonlyDirective } from './customdirectives/numberonly.directive';
+import { HighlightDirective } from './customdirectives/highlight.directive';
 
 @NgModule({
   // components, directives , pipes
@@ -17,14 +27,24 @@ import { CarouselComponent } from './carousel/carousel.component';
     FooterComponent,
     NavbarComponent,
     CarouselComponent,
+    DatabindingComponent,
+    DirectivesComponent,
+    ProductsComponent,
+    EmployeesComponent,
+    NumberonlyDirective,
+    HighlightDirective,
   ],
   // modules
   imports: [
-    BrowserModule
+    BrowserModule,
+    LoginModule,
+    FormsModule,
+    NgxPaginationModule,
+    Ng2SearchPipeModule,
   ],
   // Services/ Injectables
   providers: [],
   // components to bootstrap/load
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/products/products.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import * as data from './products.json';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css'],
+})
+export class ProductsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  products = (data as any).default;
+
+  p: any;
+  searchText: string = '';
+
+  SortProducts(type: string) {
+    if (type === 'asc') {
+      this.products.sort((p1: any, p2: any) => p1.price - p2.price);
+    } else {
+      this.products.sort((p1: any, p2: any) => p2.price - p1.price);
+    }
+  }
+  addToCart() {
+    // alert('Your Item is added to Cart');
+    Swal.fire('Good Job!', 'Your Item is added to cart', 'success');
+  }
+}
